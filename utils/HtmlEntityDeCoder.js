be11babcee85440bcca15c7669c1833cade74e07
Tanarak Chunsanit <tanarak.chuns@gmail.com>
2024-03-07 10:07:28 +0700

    add comments

--- utils/HtmlEntityDeCoder.js
+++ utils/HtmlEntityDeCoder.js
@@ -1,5 +1,8 @@
 function HtmlEntityDeCoder(text){
+    //Create textarea
     const _textArea = document.createElement('textarea')
+    //Put Html Entity to InnerHtml it will autometically decoded
     _textArea.innerHTML = text
+    //Return textarea's value
     return _textArea.value
 }
